e6f73824502102d219ce15c9d68b1fd0160c20d0
Wisanu Black <wisanu.black@outlook.com>
2023-03-31 13:14:20 +0700

    black

--- custom.js
+++ custom.js
@@ -1,1 +1,7 @@
-$(function () {});
+$(function () {
+  $('.navbar li a').each(function () {
+    if ($(this).prop('href') == window.location.href) {
+      $(this).addClass('active');
+    }
+  });
+});
